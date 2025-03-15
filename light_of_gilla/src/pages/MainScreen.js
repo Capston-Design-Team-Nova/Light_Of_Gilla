32229eb6a6ab6c86685ceeb7e0e539026113fcd4
nykim <ilsnr530@naver.com>
2025-03-15 22:57:30 +0900

    메인화면 업데이트

--- light_of_gilla/src/pages/MainScreen.js
+++ light_of_gilla/src/pages/MainScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Main, Center, P } from '../styles/MainScreenStyles';
+import { Main, Center, P, FeaturesSection, Feature, FeaturesContainer, Footer } from '../styles/MainScreenStyles';
 import Header from '../components/Header';
 
 function MainScreen() {
@@ -7,10 +7,27 @@ function MainScreen() {
     return (
         <Main>
             <Header />
-            <Center>
-                <P>GILLA</P>
+            <Center>                                
                 
             </Center>
+            <FeaturesSection>
+                <h2>GILLA 소개</h2>
+                <FeaturesContainer>
+                <Feature>
+                    <h3>기능 1</h3>
+                    <P>이곳에서 첫 번째 기능을 설명합니다.</P>
+                </Feature>
+                <Feature>
+                    <h3>기능 2</h3>
+                    <p>이곳에서 두 번째 기능을 설명합니다.</p>
+                </Feature>
+                <Feature>
+                    <h3>기능 3</h3>
+                    <p>이곳에서 세 번째 기능을 설명합니다.</p>
+                </Feature>
+                </FeaturesContainer>
+            </FeaturesSection>
+            <Footer>{/* TEAM NOVA FROM HANSUNG UNIVERSITY, SEOUL, REPUBLIC OF KOREA \n */}  ©2025 LIGHT OF GILLA ALL RIGHTS RESERVED. </Footer>
         </Main>
     );
 }
