4149452208904fb9b2375af50662542d6a79bf87
jmlee0622 <140807825+jmlee0622@users.noreply.github.com>
2025-04-07 15:09:19 +0900

    수정

--- light_of_gilla/src/pages/CommunityPage/Community.js
+++ light_of_gilla/src/pages/CommunityPage/Community.js
@@ -31,7 +31,7 @@ function Community() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get("http://qbvq3zqekb.execute-api.ap-northeast-2.amazonaws.com/post/view");
+        const response = await axios.get(`http://3.36.161.179:8082/post/view`);
         console.log("게시글 데이터를 불러오는 중");
         setPosts(response.data);
       } catch (error) {
@@ -45,7 +45,7 @@ function Community() {
     const category = encodeURIComponent(value);
     try {
       const response = await axios.get(
-        `http://qbvq3zqekb.execute-api.ap-northeast-2.amazonaws.com/post/category/${category}`
+        `http://3.36.161.179:8082/post/category/${category}`
       );
       setPosts(response.data);
     } catch (error) {
@@ -59,7 +59,7 @@ function Community() {
       try {
        
         const response = await axios.get(
-          `http://qbvq3zqekb.execute-api.ap-northeast-2.amazonaws.com/post/search/${searchString}`
+          `http://3.36.161.179:8082/post/search/${searchString}`
         );
         setPosts(response.data);
       } catch (error) {
@@ -67,7 +67,7 @@ function Community() {
       }
     } else {
       try {
-        const response = await axios.get(`http://qbvq3zqekb.execute-api.ap-northeast-2.amazonaws.com/post/`);
+        const response = await axios.get(`http://3.36.161.179:8082/post/view`);
         console.log("게시글 데이터를 불러오는 중");
         setPosts(response.data);
       } catch (error) {
