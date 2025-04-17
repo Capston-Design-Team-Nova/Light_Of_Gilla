07b2cb89d3832552c169783d67aa861ff3f6b09e
nykim <ilsnr530@naver.com>
2025-04-17 22:29:21 +0900

    게시판 내가쓴 글

--- light_of_gilla/src/pages/CommunityPage/MyArticles.js
+++ light_of_gilla/src/pages/CommunityPage/MyArticles.js
@@ -1,19 +1,36 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Header from '../../components/Header';
 import { Main, Center, Content, Button,TopRow,ToggleButton,CommunityListWrapper } from '../../styles/CommunityStyles';
 import { Link } from "react-router-dom";
-//import CommunityList from './CommunityList';
+import CommunityList from './CommunityList';
 import Sidebar from '../../components/Sidebar';
 import SearchField from '../../components/SearchField';
-//import Pagination from "../../components/Pagination";
+import Pagination from "../../components/Pagination";
+import axios from 'axios';
 
 function MyArticles() {
     const [isSidebarOpen, setSidebarOpen] = useState(false);
+    const [posts, setPosts] = useState([]);
 
     // Toggle sidebar visibility
     const toggleSidebar = () => {
         setSidebarOpen(!isSidebarOpen);
     };
+    const username = localStorage.getItem("username");
+
+    useEffect(() => {
+      const fetchMyPosts = async () => {
+        try {
+          const response = await axios.get("https://qbvq3zqekb.execute-api.ap-northeast-2.amazonaws.com/post/view");
+          const myPosts = response.data.filter(post => post.author === username); // author 대신 userId일 수도 있음
+          setPosts(myPosts);
+        } catch (error) {
+          console.error("내 글 불러오기 실패:", error);
+        }
+      };
+  
+      fetchMyPosts();
+    }, [username]);
 
     {/*const handleSearch = async (term) => {
         setSearchTerm(term);
@@ -37,13 +54,13 @@ function MyArticles() {
             console.error("게시글 데이터를 불러오는 중 오류 발생:", error);
           }
         }
-      }; */}
+      };*/}
   // 페이지네이션 상태와 로직 추가
-  //const [currentPage, setCurrentPage] = useState(1);
-  //const postsPerPage = 10; /*한 페이지에 글 10개씩 보여주기*/
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10; /*한 페이지에 글 10개씩 보여주기*/
   
   // 현재 페이지의 게시글 계산
- {/*} const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
   
@@ -54,7 +71,7 @@ function MyArticles() {
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
     window.scrollTo(0, 0); // 선택 시 스크롤 맨 위로
-  };*/}
+  };
     return (
         <Main>
             <Header />
@@ -72,18 +89,18 @@ function MyArticles() {
                 </TopRow>
                 
                 <Content isSidebarOpen={isSidebarOpen}>
-                    {/*<CommunityListWrapper>
-                        <CommunityList posts={currentPosts} />
+                    <CommunityListWrapper>
+                      <CommunityList posts={currentPosts} />
                     </CommunityListWrapper>
                     
                     {/* 페이지네이션 */}
-                    {/*{totalPages > 0 && (
+                    {totalPages > 0 && (
                         <Pagination
                             currentPage={currentPage}
                             totalPages={totalPages}
                             onPageChange={handlePageChange}
                         />
-                    )}*/}           
+                    )}           
                 </Content>
             </Center>
         </Main>
