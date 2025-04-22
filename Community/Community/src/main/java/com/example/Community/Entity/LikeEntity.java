a1531fae2d6dc4c057dbc12633ed61d493d67c9f
jmlee0622 <140807825+jmlee0622@users.noreply.github.com>
2025-04-22 21:02:11 +0900

    코드 수정

--- Community/Community/src/main/java/com/example/Community/Entity/LikeEntity.java
+++ Community/Community/src/main/java/com/example/Community/Entity/LikeEntity.java
@@ -1,6 +1,8 @@
 package com.example.Community.Entity;
 
 
+import com.example.Community.Dto.LikeDTO;
+import com.example.Community.Dto.UserDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,4 +22,10 @@ public class LikeEntity {
     @Column
     private Long post_id;
 
+    public static LikeEntity toSaveLikeEntity(LikeDTO likeDTO) {
+        LikeEntity likeEntity = new LikeEntity();
+        likeEntity.setNickName(likeDTO.getNickName());
+        likeEntity.setPost_id(likeDTO.getPost_id());
+        return likeEntity;
+    }
 }
